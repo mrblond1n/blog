705f20f569aebbd9e2bb359f68d56c7caf47b0f3
mrblond1n <mrblond1n@yandex.ru>
2022-04-17 14:24:23 +0400

    upd effector

--- features/post/model/index.ts
+++ features/post/model/index.ts
@@ -1,4 +1,4 @@
-import {forward, guard} from 'effector';
+import {forward, guard, sample} from 'effector';
 import {createGate} from 'effector-react';
 import {getPostFx} from 'features/post/model/effects';
 import {setMode} from 'features/post/model/events';
@@ -6,7 +6,7 @@ import {setMode} from 'features/post/model/events';
 export const Gate = createGate<{id?: string}>();
 const $id = Gate.state.map(x => x?.id);
 
-guard({
+sample({
     clock: [
         Gate.open,
         guard({
