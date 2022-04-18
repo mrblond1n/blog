52cd77677ee73d5adc833c0dcc2d8b728308be01
mrblond1n <mrblond1n@yandex.ru>
2022-04-18 17:00:15 +0400

    used UpdateButtonContainer instead just button in Posts page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,21 +1,18 @@
 import {useGate} from 'effector-react';
 import {ContentContainer} from 'features/posts/containers/ContentContainer';
+import {UpdateButtonContainer} from 'features/posts/containers/UpdateButtonContainer';
 import {Gate} from 'features/posts/model';
-import {onClick} from 'features/posts/model/events';
 import React from 'react';
-import {Button} from 'ui/atoms/Button';
 import {Page} from 'ui/organisms/Page';
 
 export default () => {
     useGate(Gate);
 
-    const handleClick = React.useCallback(() => onClick(), []);
-
     return (
         <Page>
             <h1>{'POSTS'}</h1>
 
-            <Button onClick={handleClick}>{'Update posts'}</Button>
+            <UpdateButtonContainer />
 
             <ContentContainer />
         </Page>
