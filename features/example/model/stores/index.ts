2c2e78d96689bdcde245a0f996c75a60182457e2
mrblond1n <mrblond1n@yandex.ru>
2022-04-18 16:58:18 +0400

    removed example feature

--- features/example/model/stores/index.ts
+++
@@ -1,6 +0,0 @@
-import {decrementScore, incrementScore} from 'features/example/model/events';
-import {createStore} from 'effector';
-
-export const $score = createStore<number>(0)
-    .on(incrementScore, value => value + 1)
-    .on(decrementScore, value => value - 1);
