59a7102cb43a4c67cd1ec64b89ca3c33ddfcf10a
mrblond1n <mrblond1n@yandex.ru>
2022-04-18 17:03:57 +0400

    added test for button on Posts page

--- __tests__/pages/Posts.test.tsx
+++ __tests__/pages/Posts.test.tsx
@@ -1,12 +1,24 @@
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {INTL} from 'constants/intl';
 import Posts from 'pages/posts';
 import React from 'react';
+import {intl} from 'utils/intl';
 
-describe('<Post />', () => {
+describe('<Posts />', () => {
     test('should display posts page', async () => {
         render(<Posts />);
         const divElement = screen.getByRole('heading');
 
         expect(divElement.innerHTML).toBe('POSTS');
+    });
+
+    test('disable button after click', () => {
+        render(<Posts />);
+        const buttonElement = screen.getByText(intl(INTL.POSTS.UPDATE));
+
+        userEvent.click(buttonElement);
+
+        expect(buttonElement).toHaveAttribute('disabled');
     });
 });
