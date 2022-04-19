bfb912a15485a5151d4e5252059bda43d6e00c93
mrblond1n <mrblond1n@yandex.ru>
2022-04-19 15:58:01 +0400

    added logout button

--- features/auth/model/effects/index.ts
+++ features/auth/model/effects/index.ts
@@ -1,10 +1,16 @@
-import {signInRequest, signUpRequest} from 'features/auth/model/requests';
+import {signInRequest, signOutRequest, signUpRequest} from 'features/auth/model/requests';
 import {UserCodec} from 'types/dtos/user.dto';
 import {createFirebaseAuthEffect} from 'utils/requestEffect';
+import {t} from 'utils/validation';
 
 export const signInFx = createFirebaseAuthEffect({
     codec: UserCodec,
     request: signInRequest,
+});
+
+export const signOutFx = createFirebaseAuthEffect({
+    codec: t.boolean,
+    request: signOutRequest,
 });
 
 export const signUpFx = createFirebaseAuthEffect({
