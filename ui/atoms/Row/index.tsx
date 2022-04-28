11a9772b95d9b5b39aec08618eb498ea1c18177c
nikita.kirdiapin <nikita.kirdiapin@gmail.com>
2022-04-28 12:15:09 +0200

    added common Row component

---
+++ ui/atoms/Row/index.tsx
@@ -0,0 +1,36 @@
+import classnames from 'classnames';
+import React from 'react';
+
+import style from './style.module.css';
+
+type TProps = {
+    alignItems?: 'center' | 'flex-start' | 'flex-end';
+    children: React.ReactNode;
+    className?: string;
+    direction?: 'row' | 'column' | 'column-reverse' | 'row-reverse';
+    fullWidth?: boolean;
+    justifyContent?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around';
+    wrap?: boolean;
+};
+
+export const Row = React.memo(
+    ({alignItems, children, className, direction, fullWidth, justifyContent, wrap}: TProps) => {
+        const directionClass = direction && style[('direction-' + direction) as 'directionRow'];
+        const alignItemsClass = alignItems && style[('align-items-' + alignItems) as 'alignItemsCenter'];
+        const justifyContentClass =
+            justifyContent && style[('justify-content-' + justifyContent) as 'justifyContentCenter'];
+        const wrapClass = wrap && style.wrap;
+
+        const classNames = classnames(
+            style.container,
+            wrapClass,
+            directionClass,
+            alignItemsClass,
+            justifyContentClass,
+            fullWidth && style.fullWidth,
+            className
+        );
+
+        return <div className={classNames}>{children}</div>;
+    }
+);
