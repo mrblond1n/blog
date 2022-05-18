0c95a228a629d726b802c0c7a425fb157647e246
mrblond1n <mrblond1n@yandex.ru>
2022-05-18 11:38:09 +0200

    removed unused components

--- features/signin/ui/atoms/SwitchButtonWrapper/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import style from './style.module.css';
-
-export const SwitchButtonWrapper = React.memo(({children}) => <div className={style.container}>{children}</div>);
