3537aaa2303510eae3fba9a697cd46f47db294ad
mrblond1n <mrblond1n@yandex.ru>
2022-05-18 11:54:22 +0200

    used change-submit-button-text api

--- features/form/containers/FormContainer.tsx
+++ features/form/containers/FormContainer.tsx
@@ -1,14 +1,16 @@
-import {useGate, useList} from 'effector-react';
+import {useGate, useList, useStore} from 'effector-react';
 import {FormGate} from 'features/form/model';
 import {onChange, onSubmit} from 'features/form/model/events';
-import {$inputs} from 'features/form/model/stores';
+import {$buttonText, $inputs} from 'features/form/model/stores';
 
 import React from 'react';
+import {Button} from 'ui/atoms/Button';
 import {Input} from 'ui/atoms/Input';
 import {Form} from 'ui/molecules/Form';
 
 export const FormContainer = React.memo(() => {
     const ref = React.useRef<HTMLFormElement>(null);
+    const text = useStore($buttonText);
 
     useGate(FormGate, {form: ref});
 
@@ -21,7 +23,7 @@ export const FormContainer = React.memo(() => {
                 <Input onChange={handleChange} {...input} />
             ))}
 
-            <button type="submit">{'Отправить'}</button>
+            <Button type="submit">{text}</Button>
         </Form>
     );
 });
