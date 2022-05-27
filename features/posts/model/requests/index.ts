cad191819ba299313d20a5fb7f6e2d56517c04be
mrblond1n <mrblond1n@yandex.ru>
2022-05-27 14:58:57 +0200

    added update request for post after send comment
    update comments_count field

--- features/posts/model/requests/index.ts
+++ features/posts/model/requests/index.ts
@@ -1,7 +1,7 @@
 import {TPostDto} from 'types/dtos/posts.dto';
 import {createFirebaseRequest} from 'utils/request';
 
-export const addPostRequest = (data: Omit<TPostDto, 'id'>) =>
+export const addPostRequest = (data: Omit<TPostDto, 'id' | 'watches_count' | 'comments_count'>) =>
     createFirebaseRequest({type: 'ADD', collection: 'posts', data});
 
 export const getPostsRequest = () => createFirebaseRequest({type: 'GET_LIST', collection: 'posts'});
