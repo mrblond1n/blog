c5771c2a4b9aefd8c953615f15b01fe63d55237d
mrblond1n <mrblond1n@yandex.ru>
2022-05-27 14:58:27 +0200

    added update request for post after load
    update watches_count field

--- features/post/state/model/effects/index.ts
+++ features/post/state/model/effects/index.ts
@@ -1,8 +1,13 @@
-import {getPostRequest} from 'features/post/state/model/requests';
+import {getPostRequest, updatePostRequest} from 'features/post/state/model/requests';
 import {PostCodec} from 'types/dtos/posts.dto';
 import {createFirebaseEffect} from 'utils/requestEffect';
 
 export const getPostFx = createFirebaseEffect({
     codec: PostCodec,
     request: getPostRequest,
+});
+
+export const updatePostFx = createFirebaseEffect({
+    codec: PostCodec,
+    request: updatePostRequest,
 });
