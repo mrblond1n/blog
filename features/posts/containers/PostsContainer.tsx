bc0e8c691cd45f89f2653c65a04f52e9a576a5ef
mrblond1n <mrblond1n@yandex.ru>
2022-05-31 15:14:30 +0200

    redone post container

--- features/posts/containers/PostsContainer.tsx
+++ features/posts/containers/PostsContainer.tsx
@@ -1,15 +1,26 @@
 import {INTL} from 'constants/intl';
 import {useList, useStoreMap} from 'effector-react';
-import {removePost} from 'features/posts/model/events';
-import {$disabledIndex, $idsList, $postsIndex} from 'features/posts/model/stores';
+import {PostActionContainer} from 'features/posts/containers/PostActionContainer';
+import {$idsList, $postsIndex} from 'features/posts/model/stores';
 import {Post} from 'features/posts/ui/Post';
 import React from 'react';
 import {ROUTES} from 'routes';
-import {Button} from 'ui/atoms/Button';
 import {NavLink} from 'ui/atoms/NavLink';
+import {Row} from 'ui/atoms/Row';
 import {intl} from 'utils/intl';
 
-export const PostsContainer = React.memo(() => useList($idsList, id => <PostContainer id={id} />));
+export const PostsContainer = React.memo(() =>
+    useList($idsList, id => (
+        <div key={id} data-testid={`post_${id}`}>
+            <Row alignItems="center">
+                <PostContainer id={id} />
+                <PostActionContainer id={id} />
+            </Row>
+
+            <NavLink href={`${ROUTES.POSTS}/${id}`}>{intl(INTL.POSTS.OPEN)}</NavLink>
+        </div>
+    ))
+);
 
 export const PostContainer = React.memo(({id}: {id: string}) => {
     const post = useStoreMap({
@@ -18,22 +29,10 @@ export const PostContainer = React.memo(({id}: {id: string}) => {
         fn: (state, [id]) => state[id],
     });
 
-    const disabled = useStoreMap({
-        store: $disabledIndex,
-        keys: [id],
-        fn: (state, [id]) => state[id],
-    });
-
-    const handleClick = React.useCallback(() => removePost(post.id), [post.id]);
-    const postId = `post_${post.id}`;
-
     return (
-        <div data-testid={postId}>
+        <Row direction="column">
+            <h4>{post.title}</h4>
             <Post>{post.text}</Post>
-            <NavLink href={`${ROUTES.POSTS}/${post.id}`}>{intl(INTL.POSTS.OPEN)}</NavLink>
-            <Button disabled={disabled} onClick={handleClick}>
-                {intl(INTL.POSTS.REMOVE)}
-            </Button>
-        </div>
+        </Row>
     );
 });
