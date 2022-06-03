e837b85670bbafceb11bbd4f5220ca57317ea5ae
mrblond1n <mrblond1n@yandex.ru>
2022-06-03 11:02:22 +0200

    added request storage helpers

--- utils/requestEffect.ts
+++ utils/requestEffect.ts
@@ -1,5 +1,6 @@
 import {createEffect, Effect} from 'effector';
 import {firebaseAuthRequest, TFirebaseAuthRequestConfig} from 'utils/requestAuth';
+import {storageRequest, TStorageRequestConfig} from 'utils/requestStorage';
 import {firebaseRequest, TRequestConfig} from './request';
 import * as t from './validation';
 
@@ -31,4 +32,18 @@ export const createFirebaseAuthEffect = <Params, Codec extends t.Any>({
     return createEffect(async (params: Params) =>
         t.decode(codec, await firebaseAuthRequest(request(params), interceptor))
     );
+};
+
+type TStorageEffectParams<Codec, Params> = {
+    codec: Codec;
+    request: (params: Params) => TStorageRequestConfig;
+    interceptor?: (data: unknown) => unknown;
+};
+
+export const createStorageEffect = <Params, Codec extends t.Any>({
+    codec,
+    request,
+    interceptor,
+}: TStorageEffectParams<Codec, Params>): Effect<Params, t.TypeOf<Codec>> => {
+    return createEffect(async (params: Params) => t.decode(codec, await storageRequest(request(params), interceptor)));
 };
