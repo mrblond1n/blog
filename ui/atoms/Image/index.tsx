b5e8f084708fb699f21d4ac626ada8a4dd1d2b3e
mrblond1n <mrblond1n@yandex.ru>
2022-06-03 15:34:33 +0200

    fixed img component

--- ui/atoms/Image/index.tsx
+++ ui/atoms/Image/index.tsx
@@ -1,13 +1,12 @@
-import Image, {ImageProps} from 'next/image';
-import React from 'react';
+import React, {ImgHTMLAttributes} from 'react';
 import style from './style.module.css';
 
-export const Img = React.memo((props: ImageProps) => {
+export const Img = React.memo((props: ImgHTMLAttributes<HTMLImageElement>) => {
     const alt = props.alt || '';
 
     return (
         <div className={style.container} style={{width: `${props.width}px`, height: `${props.height}px`}}>
-            {props.src && <Image alt={alt} className={style.img} {...props} />}
+            {props.src && <img alt={alt} className={style.img} {...props} />}
         </div>
     );
 });
