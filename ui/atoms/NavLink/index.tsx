dbfe21810f33488fd026cb00663d70a54155cb92
mrblond1n <mrblond1n@yandex.ru>
2022-06-13 15:11:11 +0200

    fixed NavLink props

--- ui/atoms/NavLink/index.tsx
+++ ui/atoms/NavLink/index.tsx
@@ -1,13 +1,9 @@
+import {LinkProps} from 'next/dist/client/link';
 import Link from 'next/link';
 import React from 'react';
-import {TDeepRoutes, TRoutes} from 'routes';
 import style from './NavLink.module.css';
 
-type TProps = {
-    children: React.ReactNode;
-    href: TRoutes | TDeepRoutes;
-};
-export const NavLink = React.memo(({children, ...props}: TProps) => {
+export const NavLink = React.memo(({children, ...props}: React.PropsWithChildren<LinkProps>) => {
     return (
         <div className={style.container}>
             <Link {...props}>{children}</Link>
