b9b4e00c8f15704915756aa6085bbc561fd735e5
mrblond1n <mrblond1n@yandex.ru>
2022-06-13 15:11:43 +0200

    removed inline styles for image component

--- ui/atoms/Image/index.tsx
+++ ui/atoms/Image/index.tsx
@@ -4,9 +4,5 @@ import style from './style.module.css';
 export const Img = React.memo((props: ImgHTMLAttributes<HTMLImageElement>) => {
     const alt = props.alt || '';
 
-    return (
-        <div className={style.container} style={{width: `${props.width}px`, height: `${props.height}px`}}>
-            {props.src && <img alt={alt} className={style.img} {...props} />}
-        </div>
-    );
+    return <div className={style.container}>{props.src && <img alt={alt} className={style.img} {...props} />}</div>;
 });
