5b3dc0a0a3f1af4c63467f79070a8dd8f97d6a13
mrblond1n <mrblond1n@yandex.ru>
2022-06-16 08:42:21 +0200

    changed firestore request args

---
+++ utils/typescript/overload.ts
@@ -0,0 +1,14 @@
+export type TOverloadedReturnType<T> = T extends {
+    (...args: any[]): infer R;
+    (...args: any[]): infer R;
+    (...args: any[]): infer R;
+    (...args: any[]): infer R;
+}
+    ? R
+    : T extends {(...args: any[]): infer R; (...args: any[]): infer R; (...args: any[]): infer R}
+    ? R
+    : T extends {(...args: any[]): infer R; (...args: any[]): infer R}
+    ? R
+    : T extends (...args: any[]) => infer R
+    ? R
+    : any;
