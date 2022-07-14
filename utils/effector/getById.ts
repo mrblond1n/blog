a01110ce11b90c487a09869e45d132598dec33c0
mrblond1n <mrblond1n@yandex.ru>
2022-07-14 16:53:50 +0200

    added handler all errors

---
+++ utils/effector/getById.ts
@@ -0,0 +1,8 @@
+import {combine, Store} from 'effector';
+
+export function getById<T>($index: Store<{[id: string]: T}>, $id: Store<string>): Store<T | null> {
+    return combine($index, $id, (index, id) =>
+        // eslint-disable-next-line no-nested-ternary
+        id === null ? null : index[id] === void 0 ? null : index[id]
+    );
+}
