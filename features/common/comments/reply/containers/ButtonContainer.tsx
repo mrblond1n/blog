eb2c47a92f4dd4ce78a6ed109497bf86fa0ce6e2
mrblond1n <mrblond1n@yandex.ru>
2022-07-19 11:43:13 +0200

    ButtonLink > Button

--- features/common/comments/reply/containers/ButtonContainer.tsx
+++ features/common/comments/reply/containers/ButtonContainer.tsx
@@ -1,9 +1,15 @@
+import {INTL} from 'constants/intl';
 import {onReply} from 'features/common/comments/reply/model/events';
 import React from 'react';
-import {ButtonLink} from 'ui/atoms/ButtonLink';
+import {Button} from 'ui/atoms/Button';
+import {intl} from 'utils/intl';
 
 export const ButtonContainer = React.memo(({id}: {id: string}) => {
     const handleClick = React.useCallback(() => onReply(id), [id]);
 
-    return <ButtonLink onClick={handleClick}>{'answer'}</ButtonLink>;
+    return (
+        <Button onClick={handleClick} size="small">
+            {intl(INTL.COMMENT.ACTION.REPLY)}
+        </Button>
+    );
 });
