a197027308d459775354b9b2b6d2f96ec1e87dd6
mrblond1n <mrblond1n@yandex.ru>
2022-07-20 09:16:25 +0200

    separate components

--- features/common/comments/state/containers/CommentContainer.tsx
+++ features/common/comments/state/containers/CommentContainer.tsx
@@ -3,6 +3,7 @@ import {ButtonDislikeContainer} from 'features/common/comments/liked/containers/
 import {ButtonLikeContainer} from 'features/common/comments/liked/containers/ButtonLikeContainer';
 import {ButtonContainer} from 'features/common/comments/reply/containers/ButtonContainer';
 import {FieldContainer} from 'features/common/comments/reply/containers/FieldContainer';
+import {SendButtonContainer} from 'features/common/comments/reply/containers/SendButtonContainer';
 import {ToggleButtonContainer} from 'features/common/comments/reply/containers/ToggleButtonContainer';
 import {$commentsIndex} from 'features/common/comments/state/model/stores';
 import {CommentBodyWrapper} from 'features/common/comments/state/ui/atoms/CommentBodyWrapper';
@@ -15,9 +16,50 @@ import React from 'react';
 import {Avatar} from 'ui/atoms/Avatar';
 import {Body} from 'ui/atoms/Body';
 import {Caption} from 'ui/atoms/Caption';
+import {Row} from 'ui/atoms/Row';
 import {dateFromNow} from 'utils/date';
 
 export const CommentContainer = React.memo(({id}: {id: string}) => {
+    return (
+        <CommentWrapper>
+            <CommentAvatar id={id} />
+
+            <CommentMainWrapper>
+                <CommentHeader id={id} />
+
+                <CommentBodyContainer id={id} />
+
+                <CommentFooter id={id} />
+
+                <CommentActions id={id} />
+            </CommentMainWrapper>
+        </CommentWrapper>
+    );
+});
+
+export const CommentBodyContainer = React.memo(({id}: {id: string}) => {
+    const comment = useStoreMap({
+        store: $commentsIndex,
+        keys: [id],
+        fn: (state, [id]) => state[id],
+    });
+
+    return <CommentBodyWrapper>{comment.text}</CommentBodyWrapper>;
+});
+
+const CommentAvatar = React.memo(({id}: {id: string}) => {
+    const comment = useStoreMap({
+        store: $commentsIndex,
+        keys: [id],
+        fn: (state, [id]) => state[id],
+    });
+
+    const initials = getInitials(comment.author);
+
+    return <Avatar>{initials}</Avatar>;
+});
+
+const CommentHeader = React.memo(({id}: {id: string}) => {
     const comment = useStoreMap({
         store: $commentsIndex,
         keys: [id],
@@ -25,29 +67,31 @@ export const CommentContainer = React.memo(({id}: {id: string}) => {
     });
 
     const date = dateFromNow(comment.created_at);
-    const initials = getInitials(comment.author);
 
     return (
-        <CommentWrapper>
-            <Avatar>{initials}</Avatar>
+        <CommentHeaderWrapper>
+            <Body>{comment.author}</Body>
+            <Caption>{date}</Caption>
+        </CommentHeaderWrapper>
+    );
+});
 
-            <CommentMainWrapper>
-                <CommentHeaderWrapper>
-                    <Body>{comment.author}</Body>
-                    <Caption>{date}</Caption>
-                </CommentHeaderWrapper>
+const CommentFooter = React.memo(({id}: {id: string}) => {
+    return (
+        <CommentFooterWrapper>
+            <ButtonLikeContainer id={id} />
+            <ButtonDislikeContainer id={id} />
+            <ButtonContainer id={id} />
+        </CommentFooterWrapper>
+    );
+});
 
-                <CommentBodyWrapper>{comment.text}</CommentBodyWrapper>
-
-                <CommentFooterWrapper>
-                    <ButtonLikeContainer id={id} />
-                    <ButtonDislikeContainer id={id} />
-                    <ButtonContainer id={id} />
-                </CommentFooterWrapper>
-
-                <FieldContainer id={id} />
-                <ToggleButtonContainer id={id} />
-            </CommentMainWrapper>
-        </CommentWrapper>
+const CommentActions = React.memo(({id}: {id: string}) => {
+    return (
+        <Row direction="column" fullWidth margin>
+            <FieldContainer id={id} />
+            <SendButtonContainer id={id} />
+            <ToggleButtonContainer id={id} />
+        </Row>
     );
 });
