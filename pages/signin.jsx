5e35b2038bd7330b4c44f9a525ad24c6bc7ffa1e
mrblond1n <mrblond1n@yandex.ru>
2022-07-20 16:25:30 +0200

    fixed width of forms

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -6,6 +6,7 @@ import {FormContainer} from 'features/common/form/containers/FormContainer';
 import React from 'react';
 import {SubmitButtonContainer} from 'features/signin/containers/SubmitButtonContainer';
 import {SectionTemplate} from 'ui/templates/SectionTemplate';
+import {Row} from 'ui/atoms/Row';
 
 export default () => {
     useGate(Gate);
@@ -15,9 +16,11 @@ export default () => {
 
     return (
         <SectionTemplate title={<h1>{'SIGN IN PAGE'}</h1>}>
-            <FormContainer>
-                <SubmitButtonContainer />
-            </FormContainer>
+            <Row justifyContent="center">
+                <FormContainer>
+                    <SubmitButtonContainer />
+                </FormContainer>
+            </Row>
 
             <LinkContainer />
         </SectionTemplate>
