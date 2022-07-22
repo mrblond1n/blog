177919368ade7d6e9fbbb170fa3a3040bc2448a8
mrblond1n <mrblond1n@yandex.ru>
2022-07-22 10:46:43 +0200

    refactor

--- features/posts/model/stores/index.ts
+++ features/posts/model/stores/index.ts
@@ -1,8 +1,7 @@
 import {createStore, restore} from 'effector';
 import {$uid} from 'features/common/app/model/stores';
 import {removePostFx} from 'features/posts/model/effects';
-import {addNewPost, addPost, removePost, setMode} from 'features/posts/model/events';
-import {Gate} from 'features/posts/model/index';
+import {addNewPost, addPost, clearIndexes, removePost, setMode} from 'features/posts/model/events';
 import {TPostDto} from 'types/dtos/posts.dto';
 import {createIndex} from 'utils/stack';
 
@@ -12,7 +11,7 @@ export const $postsIndex = createStore(createIndex<TPostDto>())
     .on(addPost, (index, post) => index.set({key: post.id, value: post}))
     .on(addNewPost, (index, post) => index.set({key: post.id, value: post}))
     .on(removePostFx.doneData, (index, id) => index.remove({key: id}))
-    .on(Gate.close, index => index.clear())
+    .on(clearIndexes, index => index.clear())
     .map(value => value.getRaw());
 
 export const $disabledIndex = createStore(createIndex<boolean>())
@@ -20,16 +19,16 @@ export const $disabledIndex = createStore(createIndex<boolean>())
     .on(removePost, (index, id) => index.set({key: id, value: true}))
     .on(removePostFx.doneData, (index, id) => index.remove({key: id}))
     .on(removePostFx.fail, (index, {params}) => index.set({key: params, value: false}))
-    .on(Gate.close, index => index.clear())
+    .on(clearIndexes, index => index.clear())
     .map(value => value.getRaw());
 
 export const $ownedIndex = createStore(createIndex<boolean>())
     .on(addPost, (index, post) => index.set({key: post.id, value: post.uid === $uid.getState()}))
     .on(removePostFx.doneData, (index, id) => index.remove({key: id}))
-    .on(Gate.close, index => index.clear())
+    .on(clearIndexes, index => index.clear())
     .map(value => value.getRaw());
 
 export const $idsList = createStore<string[]>([])
     .on([addPost, addNewPost], (state, {id}) => (state.includes(id) ? state : [id, ...state]))
     .on(removePostFx.doneData, (state, id) => state.filter(item => item !== id))
-    .reset(Gate.close);
+    .reset(clearIndexes);
