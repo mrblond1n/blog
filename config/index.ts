46d71402cbdea164317fe0c881f69ced138ad4af
mrblond1n <mrblond1n@yandex.ru>
2022-07-26 12:00:39 +0200

    fixed config for safety download project

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app';
+import {getApp, getApps, initializeApp} from 'firebase/app';
 import {Auth, connectAuthEmulator, getAuth, User} from 'firebase/auth';
 import {connectFirestoreEmulator, getFirestore} from 'firebase/firestore';
 import {connectStorageEmulator, getStorage} from 'firebase/storage';
@@ -12,15 +12,15 @@ const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY || 'mock_key',
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
     databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID || 'local',
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || 'local',
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 
 export const auth = getAuth(app);
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Getting Started
+
+- Install [node js](https://nodejs.dev)
+- Download project
+- Install project dependencies ```npm ci```
+
+### Simple start
+```npm run dev``` [site](http://localhost:3000)
+
+### Start with admin panel
+- ```npm run start``` [site](http://localhost:3000)
+- ```npm run firebase:emulators``` [admin](http://localhost:4000)
