a2d4542ed915d23abaa60c2362d6ef0607bdc160
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2022-07-27 14:34:24 +0200

    fixed theme when first run

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,8 @@ import {Html, Head, Main, NextScript} from 'next/document';
 import React from 'react';
 
 export default function Document() {
-    const mode = 'light';
-
     return (
-        <Html data-theme={mode}>
+        <Html>
             <Head />
             <body>
                 <Main />
