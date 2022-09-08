7733e41b76e8a7131ed2e95e4ef0e0e0c8c1832c
mrblond1n <mrblond1n@yandex.ru>
2022-09-08 11:51:46 +0200

    fixed rerender like/dislike button after each click

--- features/common/comments/liked/model/events/index.ts
+++ features/common/comments/liked/model/events/index.ts
@@ -8,4 +8,7 @@ export const onDislike = createEvent<string>();
 export const setLike = createEvent<{key: string; value: string}>();
 export const setDislike = createEvent<{key: string; value: string}>();
 
+export const unsetLike = createEvent<{key: string; value: string}>();
+export const unsetDislike = createEvent<{key: string; value: string}>();
+
 export const updateCommentLikes = createEvent<{key: string; liked: string[]; disliked: string[]}>();
