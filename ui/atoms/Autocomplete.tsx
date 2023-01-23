4f11a87d25016e9dda781e35435ea56d5bfae098
mrblond1n <mrblond1n@yandex.ru>
2023-01-23 12:22:29 +0100

    added autocomplete and multipleAutocomplete components

---
+++ ui/atoms/Autocomplete.tsx
@@ -0,0 +1,25 @@
+import {TextFieldProps} from '@mui/material';
+import React from 'react';
+import {TNullable} from 'types';
+import {Autocomplete as Component} from 'ui/atoms';
+import {TextField} from 'ui/atoms/TextField';
+
+type TProps = {
+    options: string[];
+    onChange: (value: TNullable<string>) => void;
+    value?: TNullable<string>;
+    fieldProps: TextFieldProps;
+};
+export const Autocomplete = ({onChange, options, fieldProps, ...props}: TProps) => {
+    const handleChange = (_: React.SyntheticEvent<Element, Event>, value: TNullable<string>) => onChange(value);
+
+    return (
+        <Component
+            {...props}
+            onChange={handleChange}
+            options={options}
+            renderInput={params => <TextField {...params} {...fieldProps} />}
+            size="small"
+        />
+    );
+};
---
+++ ui/atoms/MultipleAutocomplete.tsx
@@ -0,0 +1,26 @@
+import {TextFieldProps} from '@mui/material';
+import React from 'react';
+import {Autocomplete as Component} from 'ui/atoms';
+import {TextField} from 'ui/atoms/TextField';
+
+type TProps = {
+    options: string[];
+    onChange: (value: string[]) => void;
+    value?: string[];
+    fieldProps?: TextFieldProps;
+};
+export const MultipleAutocomplete = ({onChange, options, fieldProps, ...props}: TProps) => {
+    const handleChange = (_: React.SyntheticEvent<Element, Event>, value: string[]) => onChange(value);
+
+    return (
+        <Component
+            {...props}
+            disableCloseOnSelect
+            multiple
+            onChange={handleChange}
+            options={options}
+            renderInput={params => <TextField {...params} {...fieldProps} />}
+            size="small"
+        />
+    );
+};
