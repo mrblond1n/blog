f5dd3ce7e8f10d4ca740a792a9bb09d5fb15ad47
mrblond1n <mrblond1n@yandex.ru>
2023-01-23 09:41:59 +0100

    updated react deps

--- ui/atoms/NavLink/index.tsx
+++ ui/atoms/NavLink/index.tsx
@@ -1,14 +1,19 @@
-import {Link as MUILink, LinkProps as MUILinkProps} from '@mui/material';
+import {LinkProps as MUILinkProps} from '@mui/material';
 import {LinkProps} from 'next/dist/client/link';
 import Link from 'next/link';
 import React from 'react';
+import {styled} from 'utils/styles';
 
 type TProps = React.PropsWithChildren<LinkProps> & MUILinkProps;
 
 export const NavLink = React.memo(({children, ...props}: TProps) => {
     return (
-        <Link as={props.as} href={props.href} passHref>
-            <MUILink color={props.color}>{children}</MUILink>
-        </Link>
+        <StyledLink href={props.href} passHref>
+            {children}
+        </StyledLink>
     );
 });
+
+const StyledLink = styled(Link)(({theme}) => ({
+    color: theme.palette.primary.main,
+}));
