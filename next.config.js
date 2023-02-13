8d98ab644c72df73b3ab967b5bc60319dcad1aa5
mrblond1n <mrblond1n@yandex.ru>
2023-02-13 13:30:26 +0100

    disabled pwa in development

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const removeImports = require('next-remove-imports')()
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
 })
 
-module.exports = withPWA({
-  ...removeImports({}),
-})
+module.exports = removeImports(withPWA())
