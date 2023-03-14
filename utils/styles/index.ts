4faa76b1df68e6c5269c6ff7735ac84c08b8ee48
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-14 14:21:22 +0100

    set to import function 'styled' from one place

--- utils/styles/index.ts
+++ utils/styles/index.ts
@@ -1,2 +1,9 @@
-export {alpha, styled, createTheme} from '@mui/material'
+// eslint-disable-next-line no-restricted-imports
+import {styled as MUIStyled} from '@mui/material'
+import {Theme} from '@mui/material/styles/createTheme'
+import {CreateMUIStyled} from '@mui/system'
+
+export {alpha, createTheme} from '@mui/material'
 export {deepOrange, deepPurple, grey} from '@mui/material/colors'
+
+export const styled: CreateMUIStyled<Theme> = (Component: any, options: any) => MUIStyled(Component, options)
