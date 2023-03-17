da63bff670a4c1d7ba41bc1af13d436e0451b9d0
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-17 15:47:25 +0100

    added resetting of menu store

--- features/common/comments/menu/model/store/index.ts
+++ features/common/comments/menu/model/store/index.ts
@@ -3,11 +3,13 @@ import {$uid} from 'features/app/model/stores'
 import {closeMenu, openMenu, resetMenus} from 'features/common/comments/menu/model/events'
 import {addReply} from 'features/common/comments/reply/model/events'
 import {addComment} from 'features/common/comments/state/model/events'
+import {signOutFx} from 'features/pages/signin/model/effects'
 import {createIndex} from 'utils/stack'
 
 export const $accessToMenuIndex = createStore(createIndex<boolean>())
   .on(addComment, (index, {id, uid}) => index.set({key: id, value: uid === $uid.getState()}))
   .on(addReply, (index, {id}) => index.set({key: id, value: true}))
+  .reset(signOutFx.doneData)
   .map(value => value.getRaw())
 
 export const $openMenuIndex = createStore(createIndex<boolean>())
