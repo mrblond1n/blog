72a81807e3d1f0f604f1eea61380a12034d22889
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-17 18:46:09 +0100

    updated header styles

--- features/app/containers/HeaderContainer.tsx
+++ features/app/containers/HeaderContainer.tsx
@@ -1,3 +1,5 @@
+import {Stack} from 'ui/atoms/Stack'
+import {styled} from 'utils/styles'
 import {Wrapper} from 'features/app/ui/atoms/Wrapper'
 import {LoginButtonContainer} from 'features/pages/signin/containers/LoginButtonContainer'
 import {LogoutButtonContainer} from 'features/pages/signin/containers/LogoutButtonContainer'
@@ -6,10 +8,20 @@ import React from 'react'
 
 export const HeaderContainer = React.memo(() => {
   return (
-    <Wrapper>
-      <LoginButtonContainer />
-      <LogoutButtonContainer />
-      <ThemeSwitcherButtonContainer />
+    <Wrapper mb={2}>
+      <StyledHeader height={40} justifyContent="flex-end" pr={2}>
+        <LoginButtonContainer />
+        <LogoutButtonContainer />
+        <ThemeSwitcherButtonContainer />
+      </StyledHeader>
     </Wrapper>
   )
 })
+
+const StyledHeader = styled(Stack)(({theme}) => ({
+  position: 'fixed',
+  top: '0',
+  width: '100%',
+  zIndex: 1,
+  background: theme.palette.background.paper,
+}))
