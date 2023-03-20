d42271773363c6ec9a14522b86535f8dd723216e
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-20 16:18:27 +0100

    fixed navigation and used const for transitions

--- features/app/containers/HeaderContainer.tsx
+++ features/app/containers/HeaderContainer.tsx
@@ -1,24 +1,24 @@
 import {Logo} from 'features/app/ui/atoms/Logo'
-import {useRouter} from 'next/router'
-import {ROUTES} from 'routes'
-import {Stack} from 'ui/atoms/Stack'
-import {styled} from 'utils/styles'
 import {Wrapper} from 'features/app/ui/atoms/Wrapper'
 import {LoginButtonContainer} from 'features/pages/signin/containers/LoginButtonContainer'
 import {LogoutButtonContainer} from 'features/pages/signin/containers/LogoutButtonContainer'
 import {ThemeSwitcherButtonContainer} from 'features/theme/container/ThemeSwitcherButtonContainer'
 import React from 'react'
+import {ROUTES} from 'routes'
+import {NavLink} from 'ui/atoms/NavLink'
+import {Stack} from 'ui/atoms/Stack'
+import {styled} from 'utils/styles'
 
 export const HeaderContainer = React.memo(() => {
-  const router = useRouter()
-
-  const handleClick = () => router.push(ROUTES.HOME)
-
   return (
     <Wrapper>
       <StyledHeader justifyContent="flex-end" p={2}>
-        <Logo onClick={handleClick} />
+        <NavLink href={ROUTES.HOME}>
+          <Logo />
+        </NavLink>
+
         <div style={{width: '100%'}} />
+
         <LoginButtonContainer />
         <LogoutButtonContainer />
         <ThemeSwitcherButtonContainer />
